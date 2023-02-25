ba6d3703e4da8bcfdc28ba4a5f5fcd4232414f50
Alex <alexsandrojr.303@gmail.com>
2023-02-24 23:25:41 -0300

    feat: styles

--- src/components/ButtonConfirm/styles.ts
+++ src/components/ButtonConfirm/styles.ts
@@ -1,17 +1,20 @@
 import { StyleSheet } from "react-native";
 
+import colors from "../../constants/colors";
+
 const styles = StyleSheet.create({
   button: {
-    backgroundColor: "orange",
+    backgroundColor: colors.main["orange"],
     padding: 10,
     alignItems: "center",
     justifyContent: "center",
     borderRadius: 6,
+    height: 45,
   },
   textButton: {
     color: "white",
-    fontSize: 14,
-    fontWeight: "600",
+    fontSize: 16,
+    fontWeight: "700",
   },
 });
 
