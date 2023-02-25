b78f58ffb26d1e84c7ac0bea6906350505fa59a1
Alex <alexsandrojr.303@gmail.com>
2023-02-24 22:12:00 -0300

    feat: styles

--- src/components/ButtonConfirm/index.tsx
+++ src/components/ButtonConfirm/index.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { Text, TouchableOpacity, TouchableOpacityProps } from "react-native";
 
+import styles from "./styles";
+
 type IButtonProps = {
   children: string;
 } & TouchableOpacityProps;
 
 export default function ButtonConfirm({ children, ...rest }: IButtonProps) {
   return (
-    <TouchableOpacity {...rest}>
-      <Text>{children}</Text>
+    <TouchableOpacity style={styles.button} {...rest}>
+      <Text style={styles.textButton}>{children}</Text>
     </TouchableOpacity>
   );
 }
---
+++ src/components/ButtonConfirm/styles.ts
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: "blue",
+    padding: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 6,
+  },
+  textButton: {
+    color: "white",
+    fontSize: 14,
+    fontWeight: "600",
+  },
+});
+
+export default styles;
