3a7e2ee3a186713f87c5cc340666c88de993d8e0
Alex <alexsandrojr.303@gmail.com>
2023-02-24 21:53:04 -0300

    feat: add useImperativeHandle in TextInputWithLabel

--- src/components/TextInputWithLabel/index.tsx
+++ src/components/TextInputWithLabel/index.tsx
@@ -1,18 +1,54 @@
-import React from "react";
+import React, {
+  forwardRef,
+  useImperativeHandle,
+  useRef,
+  useState,
+} from "react";
 import { View, Text, TextInput, TextInputProps } from "react-native";
+
+export type InputValueRef = {
+  value: string;
+  focus?: () => void;
+  blur: () => void;
+  setValue?: (value: string) => void;
+} & TextInputProps;
 
 type ITextInputProps = {
   label: string;
 } & TextInputProps;
 
-export default function TextInputWithLabel({
-  label,
-  ...rest
-}: ITextInputProps) {
+const TextInputWithLabel: React.ForwardRefRenderFunction<
+  InputValueRef,
+  ITextInputProps
+> = ({ label, onChangeText, ...rest }: ITextInputProps, ref) => {
+  const internalRef = useRef<TextInput>(null);
+  const [text, setText] = useState("");
+
+  useImperativeHandle(
+    ref,
+    () => ({
+      value: text,
+      focus: () => internalRef.current?.focus?.(),
+      blur: () => internalRef.current?.blur?.(),
+      setValue: (value: string) => setText(value),
+    }),
+    [text]
+  );
+
   return (
     <View>
       <Text>{label}</Text>
-      <TextInput {...rest} />
+      <TextInput
+        ref={internalRef}
+        value={text}
+        onChangeText={(value: string) => {
+          setText(value);
+          onChangeText?.(value);
+        }}
+        {...rest}
+      />
     </View>
   );
-}
+};
+
+export default forwardRef(TextInputWithLabel);
