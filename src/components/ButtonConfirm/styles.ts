fad119780e6cb1cb5251b2785943a9d9f666c2c6
Alex <alexsandrojr.303@gmail.com>
2023-02-24 22:41:23 -0300

    feat: change button color

--- src/components/ButtonConfirm/styles.ts
+++ src/components/ButtonConfirm/styles.ts
@@ -2,7 +2,7 @@ import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
   button: {
-    backgroundColor: "blue",
+    backgroundColor: "orange",
     padding: 10,
     alignItems: "center",
     justifyContent: "center",
