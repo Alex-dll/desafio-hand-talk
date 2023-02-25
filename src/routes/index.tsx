b205b04133f08e58fd85fede8b555a8fdf3c1d2b
Alex <alexsandrojr.303@gmail.com>
2023-02-24 22:10:40 -0300

    feat: use authStore to define routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,11 +2,12 @@ import React from "react";
 
 import SignInNavigator from "./SignIn";
 import SignedInNavigator from "./SignedIn";
+import { useAuthStore } from "../store/authStore";
 
 const Routes: React.FC = () => {
-  const isSignedIn = false;
+  const auth = useAuthStore((state) => state.auth);
 
-  return isSignedIn ? <SignedInNavigator /> : <SignInNavigator />;
+  return auth ? <SignedInNavigator /> : <SignInNavigator />;
 };
 
 export default Routes;
