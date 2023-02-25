1184761308a1cebf8de9a2c7df4a8922e3e53ce2
Alex <alexsandrojr.303@gmail.com>
2023-02-24 23:38:14 -0300

    feat: loading to button

--- src/components/ButtonConfirm/index.tsx
+++ src/components/ButtonConfirm/index.tsx
@@ -1,16 +1,30 @@
 import React from "react";
-import { Text, TouchableOpacity, TouchableOpacityProps } from "react-native";
+import {
+  Text,
+  TouchableOpacity,
+  TouchableOpacityProps,
+  ActivityIndicator,
+} from "react-native";
 
 import styles from "./styles";
 
 type IButtonProps = {
   children: string;
+  isLoading?: boolean;
 } & TouchableOpacityProps;
 
-export default function ButtonConfirm({ children, ...rest }: IButtonProps) {
+export default function ButtonConfirm({
+  children,
+  isLoading,
+  ...rest
+}: IButtonProps) {
   return (
     <TouchableOpacity style={styles.button} {...rest}>
-      <Text style={styles.textButton}>{children}</Text>
+      {isLoading ? (
+        <ActivityIndicator size="small" color="#fff" />
+      ) : (
+        <Text style={styles.textButton}>{children}</Text>
+      )}
     </TouchableOpacity>
   );
 }
