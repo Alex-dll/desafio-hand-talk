60632154a9b385cbec5b7a1f307d797c09d8f088
Alex <alexsandrojr.303@gmail.com>
2023-02-26 17:55:11 -0300

    refactor: inputColor

--- src/components/InputColor/index.tsx
+++ src/components/InputColor/index.tsx
@@ -1,15 +1,25 @@
 import React from "react";
+import type { RefCallBack } from "react-hook-form";
 import { TextInput } from "react-native";
 import type { TextInputProps } from "react-native";
 
 import { styles } from "./styles";
 
 type Props = {
-  value?: string;
+  ref?: RefCallBack;
 } & TextInputProps;
 
-function InputColor({ ...rest }: Props) {
-  return <TextInput {...rest} style={styles.inputContainer} />;
+function InputColor({ ref, ...rest }: Props) {
+  return (
+    <TextInput
+      ref={ref}
+      {...rest}
+      style={styles.inputContainer}
+      autoCapitalize="none"
+      autoCorrect={false}
+      keyboardType="default"
+    />
+  );
 }
 
 export default InputColor;
