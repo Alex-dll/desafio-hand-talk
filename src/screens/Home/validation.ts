0e1754563f01fd42a8a1412b88ca8ef71126a7aa
Alex <alexsandrojr.303@gmail.com>
2023-02-26 17:55:42 -0300

    feat: use verifyIfColorIsValid

---
+++ src/screens/Home/validation.ts
@@ -0,0 +1,21 @@
+import * as yup from "yup";
+
+import { verifyIfColorIsValid } from "../../utils/verifyIfColorIsValid";
+
+export const schemaColors = yup.object().shape({
+  cube: yup
+    .string()
+    .test("Color test", "Cor do cubo invalida", (value) =>
+      verifyIfColorIsValid(value)
+    ),
+  cone: yup
+    .string()
+    .test("Color test", "Cor do cone invalida", (value) =>
+      verifyIfColorIsValid(value)
+    ),
+  dodecahedron: yup
+    .string()
+    .test("Color test", "Cor do dodecaedro invalida", (value) =>
+      verifyIfColorIsValid(value)
+    ),
+});
