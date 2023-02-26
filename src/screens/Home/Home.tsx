8e2824b81984125deb06d489ff132e9231dbe2e9
Alex <alexsandrojr.303@gmail.com>
2023-02-26 13:34:50 -0300

    feat: add inputs to change color in home

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,28 +1,46 @@
 import { Canvas } from "@react-three/fiber";
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { SafeAreaView, useWindowDimensions } from "react-native";
+import {
+  KeyboardAvoidingView,
+  SafeAreaView,
+  useWindowDimensions,
+  View,
+} from "react-native";
 
 import styles from "./styles";
 import Box from "../../components/Box";
+import ButtonConfirm from "../../components/ButtonConfirm";
 import Cone from "../../components/Cone";
 import Dodecahedron from "../../components/Dodecahedron";
+import InputColor from "../../components/InputColor";
 
 export default function Home() {
   const { height, width } = useWindowDimensions();
   const canvasHeight = (75 / 100) * height;
 
   return (
-    <SafeAreaView style={styles.container}>
-      <StatusBar style="light" />
-      <Canvas style={{ height: canvasHeight, width }}>
-        <ambientLight />
-        <pointLight position={[10, 10, 10]} />
+    <KeyboardAvoidingView behavior="padding" style={{ flex: 1 }}>
+      <SafeAreaView style={styles.container}>
+        <StatusBar style="light" />
+        <Canvas style={{ height: canvasHeight, width }}>
+          <ambientLight />
+          <pointLight position={[10, 10, 10]} />
 
-        <Box position={[0, +1.5, 0]} color="red" />
-        <Cone position={[0, 0, 0]} color="yellow" />
-        <Dodecahedron position={[0, -1.5, 0]} color="green" />
-      </Canvas>
-    </SafeAreaView>
+          <Box position={[0, +1.5, 0]} color="red" />
+          <Cone position={[0, 0, 0]} color="yellow" />
+          <Dodecahedron position={[0, -1.5, 0]} color="green" />
+        </Canvas>
+
+        <View style={styles.wrapper}>
+          <View style={styles.inputContainer}>
+            <InputColor placeholder="Cor do Cubo" />
+            <InputColor placeholder="Cor do Cone" />
+            <InputColor placeholder="Cor do Dodecaedro" />
+          </View>
+          <ButtonConfirm>Aplicar</ButtonConfirm>
+        </View>
+      </SafeAreaView>
+    </KeyboardAvoidingView>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -5,6 +5,15 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "black",
   },
+  wrapper: {
+    paddingHorizontal: 5,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-around",
+    marginBottom: 10,
+  },
 });
 
 export default styles;
