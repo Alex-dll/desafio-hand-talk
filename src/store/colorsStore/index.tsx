610ac8a44f109f9fcf9aa241ff9aa08e923209d4
Alex <alexsandrojr.303@gmail.com>
2023-02-26 23:16:34 -0300

    feat: get colors from firebase

--- src/store/colorsStore/index.tsx
+++ src/store/colorsStore/index.tsx
@@ -1,25 +1,39 @@
+import { Alert } from "react-native";
 import { create } from "zustand";
 
+import { getUserColorsFirebaseData } from "../../functions/getUserColorsFirebaseData";
+import { setColorsToFirebaseData } from "../../functions/setColorsToFirebaseData";
+import type { ColorsProps } from "../../types/colors";
+
 type useColorsStoreProps = {
-  colors: {
-    cube: string;
-    cone: string;
-    dodecahedron: string;
-  };
-  setColors: (colors: {
-    cube?: string;
-    cone?: string;
-    dodecahedron?: string;
-  }) => void;
+  colors: ColorsProps;
+  setColors: (colors: ColorsProps) => Promise<void>;
+  getColors: (userId: string) => Promise<void>;
 };
 
-export const useColorsStore = create<useColorsStoreProps>((set) => ({
+export const useColorsStore = create<useColorsStoreProps>((set, get) => ({
   colors: {
+    userId: null,
     cube: "red",
     cone: "yellow",
     dodecahedron: "green",
   },
-  setColors: (colors) => {
+  getColors: async (userId) => {
+    await getUserColorsFirebaseData({ userId }).then((colors) =>
+      set(() => ({ colors: { userId, ...colors } }))
+    );
+  },
+  setColors: async (colors) => {
+    for (const key in colors) {
+      if (colors[key] === "") {
+        delete colors[key];
+      }
+    }
+
     set((state) => ({ colors: { ...state.colors, ...colors } }));
+
+    await setColorsToFirebaseData(get().colors).catch((error) =>
+      Alert.alert("Erro", error.message)
+    );
   },
 }));
