9c4c21324756b0faa5877e8625cfccd34b0a4ce2
Alex <alexsandrojr.303@gmail.com>
2023-02-26 23:15:38 -0300

    feat: get colors

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import SignInNavigator from "./SignIn";
 import SignedInNavigator from "./SignedIn";
 import { useAuthStore } from "../store/authStore";
+import { useColorsStore } from "../store/colorsStore";
 
 const Routes: React.FC = () => {
   const auth = useAuthStore((state) => state.auth);
+  const [getColors] = useColorsStore((state) => [state.getColors]);
+
+  const userId = auth?.currentUser?.uid;
+
+  useEffect(() => {
+    (async () => {
+      if (userId) {
+        await getColors(userId);
+      }
+    })();
+  }, [userId]);
 
   return auth ? <SignedInNavigator /> : <SignInNavigator />;
 };
