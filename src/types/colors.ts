83c327b34b31fa9ab8b2a76679073e266382693a
Alex <alexsandrojr.303@gmail.com>
2023-02-26 23:17:26 -0300

    feat: add generic types

---
+++ src/types/colors.ts
@@ -0,0 +1,12 @@
+export type getColorToUserReturn = {
+  cube: string;
+  cone: string;
+  dodecahedron: string;
+};
+
+export type ColorsProps = {
+  userId: string;
+  cube: string;
+  cone: string;
+  dodecahedron: string;
+};
