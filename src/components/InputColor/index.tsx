aa41cdd7b15658e89590f42000c93793e098bd13
Alex <alexsandrojr.303@gmail.com>
2023-02-28 01:08:17 -0300

    feat: add forward ref

--- src/components/InputColor/index.tsx
+++ src/components/InputColor/index.tsx
@@ -1,25 +1,54 @@
-import React from "react";
-import type { RefCallBack } from "react-hook-form";
+import React, {
+  forwardRef,
+  useImperativeHandle,
+  useRef,
+  useState,
+} from "react";
 import { TextInput } from "react-native";
 import type { TextInputProps } from "react-native";
 
 import { styles } from "./styles";
 
-type Props = {
-  ref?: RefCallBack;
-} & TextInputProps;
+import type { InputValueRef } from "~/types/inputText";
 
-function InputColor({ ref, ...rest }: Props) {
+const InputColor: React.ForwardRefRenderFunction<
+  InputValueRef,
+  TextInputProps
+> = ({ onChangeText, ...rest }, ref) => {
+  const [isFocus, setIsFocus] = useState(false);
+  const [text, setText] = useState("");
+
+  const internalRef = useRef<TextInput>(null);
+
+  useImperativeHandle(
+    ref,
+    () => ({
+      value: text,
+      focus: () => internalRef.current?.focus?.(),
+      blur: () => internalRef.current?.blur?.(),
+      setValue: (value: string) => setText(value),
+    }),
+    [text]
+  );
+
+  const styleFocus = isFocus && styles.inputFocus;
+
   return (
     <TextInput
-      ref={ref}
+      ref={internalRef}
       {...rest}
-      style={styles.inputContainer}
+      style={[styles.inputContainer, styleFocus]}
       autoCapitalize="none"
       autoCorrect={false}
       keyboardType="default"
+      onChangeText={(value: string) => {
+        setText(value);
+        onChangeText?.(value);
+      }}
+      onFocus={() => setIsFocus(true)}
+      onBlur={() => setIsFocus(false)}
     />
   );
-}
+};
 
-export default InputColor;
+export default forwardRef(InputColor);
